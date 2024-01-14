323510800f4477795c337b36e69a4a934f813018
Pankaj <kumar.soft53@gmail.com>
2024-01-14 16:12:36 +0530

    4th Commit test

--- src/main/java/azure/demployment/practice/AzureDeploymentPractice/AzureDemoController.java
+++ src/main/java/azure/demployment/practice/AzureDeploymentPractice/AzureDemoController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AzureDemoController {
     @GetMapping("/say")
     public String display(){
-        System.out.println("Changes in First Controller");
-        //Changed on git reposetory file
-        return "you are my lucy charms Merry janu";
+        return "Next project is coming soon";
     }
 }
