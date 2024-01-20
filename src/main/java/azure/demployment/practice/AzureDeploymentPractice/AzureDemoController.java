654ddb1dfb08ae95328cd0767769586e3ea67e06
kumarsoft53 <kumar.soft53@gmail.com>
2024-01-20 21:01:05 +0530

    notice add commit

--- src/main/java/azure/demployment/practice/AzureDeploymentPractice/AzureDemoController.java
+++ src/main/java/azure/demployment/practice/AzureDeploymentPractice/AzureDemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AzureDemoController {
-    @GetMapping("/notice")
+    @GetMapping("/advertise")
     public String display(){
 
         return "<!DOCTYPE html>\n" +
